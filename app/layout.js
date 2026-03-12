7d2a70fd6c85f197dfe3138b57b5a8183ee82226
Utsab Acharya <acharyautsab68@gmail.com>
2026-03-12 22:49:09 +0530

    add open graph preview image

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,30 @@ export const metadata = {
   icons: {
     icon: "/favicon.ico",
   },
+ openGraph: {
+    title: "Utsab Raj Acharya | AI & Data Science Portfolio",
+    description:
+      "AI • Data Science Enthusiast • Developer. Building intelligent systems with machine learning, data science, and modern AI technologies.",
+    url: "https://www.utsabrajacharya.com.np",
+    siteName: "Utsab Portfolio",
+    images: [
+      {
+        url: "/preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Utsab Raj Acharya AI Data Science Portfolio",
+      },
+    ],
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Utsab Raj Acharya | AI & Data Science Portfolio",
+    description:
+      "AI • Data Science Enthusiast • Developer building intelligent systems and data-driven applications.",
+    images: ["/preview.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
